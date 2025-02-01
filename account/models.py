fb15146411672852e234efa8216e6ffae8547064
hamidba <balalzadehhamid79@gmail.com>
2025-02-01 13:26:05 +0330

    add email to user

--- account/models.py
+++ account/models.py
@@ -39,6 +39,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     """Custom User Model"""
 
     phone = models.CharField(max_length=11, unique=True, validators=[phone_validator])
+    email = models.EmailField(null=True, blank=True)
     fullName = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
