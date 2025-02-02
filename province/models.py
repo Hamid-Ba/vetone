9e7a66574e091ff3731f658a3dc389e97162403e
hamidba <balalzadehhamid79@gmail.com>
2025-02-02 12:36:44 +0330

    add address model

--- province/models.py
+++ province/models.py
@@ -4,6 +4,8 @@ Province Module Models
 from django.db import models
 from django.shortcuts import reverse
 
+from common.models import BaseModel
+
 
 class Province(models.Model):
     """Province Model"""
@@ -32,3 +34,32 @@ class City(models.Model):
 
     class Meta:
         verbose_name_plural = "Cities"
+
+
+class Address(BaseModel):
+    """Address Model"""
+
+    street = models.CharField(max_length=255, blank=False, null=False)
+    city = models.ForeignKey(City, null=False, blank=False, on_delete=models.CASCADE)
+
+    # veterinarain info
+    clinic_name = models.CharField(max_length=255, blank=True, null=True)
+    google_map_url = models.URLField(
+        max_length=250,
+        blank=True,
+        null=True,
+        error_messages={"invalid": "مقدار وارد شده صحیح نم باشد"},
+    )
+
+    # rancher info
+    village_name = models.CharField(max_length=255, blank=True, null=True)
+
+    user = models.ForeignKey(
+        "account.User", on_delete=models.CASCADE, null=False, blank=False
+    )
+
+    def __str__(self):
+        return f"{self.user.phone} - {self.city.name} - {self.street}"
+
+    class Meta:
+        verbose_name_plural = "Addresses"
