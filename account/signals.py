cec3b4047d8d378d60c348d2e9a2adb5c83fda98
hamidba <balalzadehhamid79@gmail.com>
2025-03-29 11:16:03 +0330

    make transaction when user registered

--- account/signals.py
+++ account/signals.py
@@ -11,7 +11,7 @@ from .models import User
 def after_user_registration(sender, instance, created, **kwargs):
     """Create Wallet For User after Registration"""
     if created:
-        wallet = Wallet.objects.create(store=instance)
+        wallet = Wallet.objects.create(user=instance)
         wallet.charge(50000)
         if not instance.is_staff:
             Rancher.objects.get_or_create(user=instance)
